6029f75c76cb36561a96ca5d1b6d431170dd83e6
Jo Geoghegan <josephine.geoghegan@apryse.com@Jos-MacBook-Pro.local>
2025-08-20 13:47:09 +0200

    table and highlight annotation

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ const initialDoc = 'https://apryse.s3.us-west-1.amazonaws.com/public/files/sampl
 
 const disabledElements = [
   'toolbarGroup-View',
-  'toolbarGroup-Annotate',
   'toolbarGroup-Shapes',
   'toolbarGroup-Redact',
   'toolbarGroup-Insert',
@@ -16,6 +15,7 @@ const disabledElements = [
   'toolbarGroup-Measure',
   'cropToolGroupButton',
 ];
+
 const EnabledElements = [
   'toolbarGroup-EditText',
   'addParagraphToolGroupButton',
@@ -90,23 +90,34 @@ WebViewer({
         const doc = await documentViewer.getDocument().getPDFDoc();
         doc.lock();
         const page = await doc.getPage(2);
-        const data = [
-          ['Name', 'Value1', 'Value2', 'Value3', 'Value4'],
-          ['Alice', '10', '20', '39', '1'],
-          ['Bob', '30', '40', '34', '2'],
-          ['Jim', '10', '20', '23', '3'],
-          ['David', '30', '40', '22', '3']
-        ];
+        const toHighlight = [];
         const headers = Object.keys(jsonTestData[0]);
         const startX = 50, startY = 200, cellWidth = 100, cellHeight = 30;
-          for (let row = 0; row < jsonTestData.length; row++) {
+        const builder = await PDFNet.ElementBuilder.create();
+        const writer = await PDFNet.ElementWriter.create();
+        await writer.beginOnPage(page);
+        const font = await PDFNet.Font.create(doc, PDFNet.Font.StandardType1Font.e_helvetica);
+        const fontBold = await PDFNet.Font.create(doc, PDFNet.Font.StandardType1Font.e_helvetica_bold);
+        for (let col = 0; col < headers.length; col++) {
+          const x = startX + col * cellWidth;
+          const y = startY;
+          const rect = await builder.createRect(x, y, cellWidth, -cellHeight);
+          rect.setPathFill(false);
+          rect.setPathStroke(true);
+          writer.writeElement(rect);
+          const text = await builder.createTextBegin(fontBold, 12);
+          writer.writeElement(text);
+          const textElement = await builder.createTextRun(headers[col], fontBold, 12);
+          textElement.setTextMatrixEntries(1, 0, 0, 1, x + 5, y - cellHeight + 20);
+          writer.writeElement(textElement);
+          writer.writeElement(await builder.createTextEnd());
+        }
+        for (let row = 0; row < jsonTestData.length; row++) {
           for (let col = 0; col < headers.length; col++) {
             const key = headers[col];
             const value = typeof jsonTestData[row][key] === "string" ? jsonTestData[row][key] : jsonTestData[row][key].toString();
             const x = startX + col * cellWidth;
-            const y = startY - row * cellHeight;
-            const builder = await PDFNet.ElementBuilder.create();
-            const writer = await PDFNet.ElementWriter.create();
+            const y = startY - row * cellHeight - 30;
             writer.beginOnPage(page);
             const rect = await builder.createRect(x, y, cellWidth, -cellHeight);
             rect.setPathFill(false);
@@ -115,15 +126,42 @@ WebViewer({
             const font = await PDFNet.Font.create(doc, PDFNet.Font.StandardType1Font.e_helvetica);
             const text = await builder.createTextBegin(font, 12);
             writer.writeElement(text);
-            const textElement = await builder.createTextRun(value,font, 12);
+            const textElement = await builder.createTextRun(value, font, 12);
+            if (key === "Value1" && jsonTestData[row][key] > 20) {
+              console.log(jsonTestData[row]["Name"]);
+              toHighlight.push([x,y])
+            }
             textElement.setTextMatrixEntries(1, 0, 0, 1, x + 5, y - cellHeight + 20);
             writer.writeElement(textElement);
             writer.writeElement(await builder.createTextEnd());
             writer.end();
-          }}
+          }
+        }
         await doc.unlock();
-        documentViewer.refreshAll();
-        documentViewer.updateView();
+        
+        const highlightItem = (startX, startY) => {
+          documentViewer.refreshAll();
+          documentViewer.updateView();
+          const pageHeight = documentViewer.getPageHeight(2);
+          const highlightX = startX;
+          const highlightY = pageHeight - startY + cellHeight;
+          const highlightWidth = cellWidth;
+          const highlightHeight = cellHeight;
+          const highlightAnnot = new instance.Core.Annotations.RectangleAnnotation();
+          highlightAnnot.PageNumber = 2; 
+          highlightAnnot.X = highlightX;
+          highlightAnnot.Y = highlightY - highlightHeight;
+          highlightAnnot.Width = highlightWidth;
+          highlightAnnot.Height = highlightHeight;
+          highlightAnnot.StrokeColor = new instance.Core.Annotations.Color(255, 255, 0);
+          highlightAnnot.FillColor = new instance.Core.Annotations.Color(255, 255, 0);
+          highlightAnnot.Opacity = 0.3;
+          instance.Core.annotationManager.addAnnotation(highlightAnnot);
+          instance.Core.annotationManager.redrawAnnotation(highlightAnnot);
+        } 
+        toHighlight.forEach(i => {
+          highlightItem(i[0], i[1]);
+        });
       });
     });
   });
